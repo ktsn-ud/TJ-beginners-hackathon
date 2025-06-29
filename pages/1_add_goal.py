cc4e117a50aaa048f8d442bcbad9143ffe9944ed
ktsn_ud <igcz025221@gmail.com>
2025-06-29 13:17:21 +0900

    1_add_goalのsession_stateに関する修正

--- pages/1_add_goal.py
+++ pages/1_add_goal.py
@@ -10,6 +10,7 @@ init_session_state()
 st.title("大目標を作成")
 title = st.text_input("大目標のタイトル")
 due_date = st.date_input("大目標の期限", datetime.date.today())
+
 if st.button("次へ(中期目標の追加)") and title:
-    st.session_state.goal = Goal(title=title, due_date=due_date, subgoals=[])
-    st.switch_page("pagea/2_add_subgoal.py")
+    st.session_state.current_goal = Goal(title=title, due_date=due_date, subgoals=[])
+    st.switch_page("pages/2_add_subgoal.py")
